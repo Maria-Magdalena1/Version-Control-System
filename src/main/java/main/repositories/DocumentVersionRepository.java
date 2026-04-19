ea55c37a7729c3c50f54b2e943fa29aa2b26d396
Maria-Magdalena1 <mariamagdalena.vladimirova@gmail.com>
2026-04-19 13:44:22 +0300

    add new method

--- src/main/java/main/repositories/DocumentVersionRepository.java
+++ src/main/java/main/repositories/DocumentVersionRepository.java
@@ -5,6 +5,7 @@ import main.entities.VersionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -13,4 +14,6 @@ public interface DocumentVersionRepository extends JpaRepository<DocumentVersion
     Optional<DocumentVersion> findTopByDocument_DocumentIdAndStatusOrderByCreatedAtDesc(UUID documentId, VersionStatus versionStatus);
 
     Optional<DocumentVersion> findDocumentVersionByDocument_DocumentIdAndIsActiveIsTrue(UUID documentId);
+
+    List<DocumentVersion> findAllByDocument_DocumentIdOrderByCreatedAtAsc(UUID documentDocumentId);
 }

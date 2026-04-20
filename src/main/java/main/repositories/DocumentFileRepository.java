1e4c8014d84b106111bb08cf677e0c71e5e6ebd9
Maria-Magdalena1 <mariamagdalena.vladimirova@gmail.com>
2026-04-20 16:39:53 +0300

    add new method in the repo

--- src/main/java/main/repositories/DocumentFileRepository.java
+++ src/main/java/main/repositories/DocumentFileRepository.java
@@ -4,8 +4,10 @@ import main.entities.DocumentFile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface DocumentFileRepository extends JpaRepository<DocumentFile, UUID> {
+    List<DocumentFile> findByDocument_DocumentIdAndIsDeletedFalse(UUID documentId);
 }

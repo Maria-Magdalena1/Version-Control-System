e7e32d91e7435e2f9c6e774068993c617137e87b
Maria-Magdalena1 <mariamagdalena.vladimirova@gmail.com>
2026-04-20 16:38:15 +0300

    added constraints

--- src/main/java/main/web/DocumentDTO.java
+++ src/main/java/main/web/DocumentDTO.java
@@ -1,14 +1,29 @@
 package main.web;
 
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Builder
 public class DocumentDTO {
+
+    @Column(nullable = false,unique = true,length = 150)
+    @Size(min=3,max=150)
+    @NotBlank
     private String title;
+
+    @Size(max = 500)
     private String description;
+
+    @Size(max = 10000)
+    @Column(columnDefinition = "TEXT")
     private String content;
+    private String filePath;
 }
--- src/main/java/main/web/UserDTO.java
+++ src/main/java/main/web/UserDTO.java
@@ -1,6 +1,12 @@
 package main.web;
 
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import main.entities.Role;
@@ -10,14 +16,30 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Builder
 public class UserDTO {
 
+    @Column(unique = true, nullable = false, length = 20)
+    @Size(min = 3, max = 20)
     private String username;
+
+    @Column(nullable = false)
+    @Email
     private String email;
+
+    @Column(nullable = false)
     private String fullName;
+
     private Role role;
+
+    @Column(nullable = false)
+    @Size(min = 6)
+    @NotBlank
     private String password;
+
     private String confirmPassword;
+
     private LocalDateTime createdAt;
+
     private boolean isActive;
 }

ad23cd9113ce9f390049dd33d5b855c4b066388e
Maria-Magdalena1 <mariamagdalena.vladimirova@gmail.com>
2026-04-21 20:37:05 +0300

    add new method

--- src/main/java/main/repositories/DocumentFileRepository.java
+++ src/main/java/main/repositories/DocumentFileRepository.java
@@ -2,12 +2,25 @@ package main.repositories;
 
 import main.entities.DocumentFile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface DocumentFileRepository extends JpaRepository<DocumentFile, UUID> {
-    List<DocumentFile> findByDocument_DocumentIdAndIsDeletedFalse(UUID documentId);
+    @Query("""
+                SELECT df FROM DocumentFile df
+                JOIN FETCH df.uploadedBy
+                WHERE df.document.documentId = :documentId
+                AND df.isDeleted = false
+            """)
+    List<DocumentFile> findFilesByDocumentWithUser(UUID documentId);
+
+    Optional<DocumentFile> findByFileIdAndDocument_DocumentIdAndIsDeletedFalse(
+            UUID fileId,
+            UUID documentId
+    );
 }
